da20877caad34af3f2704eaeda1972b7f3952d6c
Sam Rahimi <samrahimi420@gmail.com>
2018-06-26 19:00:53 -0400

    Merging in stealth capital fixes, moving stealth to new folder structure to work with firebase hosting deploy script

--- stealth_capital/public/assets/js/price.js
+++ stealth_capital/public/assets/js/price.js
@@ -2,6 +2,7 @@
 window.ETHUSD=0;
 //Nominal order size used for calculating the quote price
 window.baseOrderSizeInEth = 0.001
+window.gasPrice = 0
 
 function updateETHUSD() {
     $.getJSON("https://api.coinmarketcap.com/v2/ticker/1027/", (result) => {
@@ -18,11 +19,26 @@ function convertToUSD(amountInEth) {
 // by requesting it from the associated exchanger.
 // Accepts: the address of the exchanger linked with the desired token
 async function getPriceQuote(exchangerAddress) {
-    let exchangerContract = eth.contract(exchangerABI, "", { "from": myAddress }).at(exchangerAddress)
+    let exchangerContract = null
+    if (!exchangeUI.readonly)
+        exchangerContract = eth.contract(exchangerABI, "", { "from": myAddress }).at(exchangerAddress)
+    else
+        exchangerContract = eth.contract(exchangerABI).at(exchangerAddress)
 
     let totalTokens = await exchangerContract.getPurchasePrice(decimalToRaw(window.baseOrderSizeInEth, 18))
     let actualTokens = rawToDecimal(totalTokens[0].toString(10), 18);
     return window.baseOrderSizeInEth / actualTokens; //If 1 eth gets you n tokens, each token is worth 1/n eth. 
+}
+
+async function updateGasPrice() {
+    web3.eth.getGasPrice((e,r) =>{
+        web3.eth.getGasPrice((e,r) => console.log("Default gas price: "+r.toString(10)))
+        let g= r / 1000000000
+        if (g <= 10) 
+            window.gasPrice = g * 3
+        else 
+            window.gasPrice = g
+    })
 }
 
 $(document).ready(() => {
