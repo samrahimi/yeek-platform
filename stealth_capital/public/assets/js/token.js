480023dcb4dcfab0ce08f58f3b4c6276f66dd018
Sam Rahimi <samrahimi420@gmail.com>
2018-06-30 13:19:28 -0400

    Chart sizing fix

--- stealth_capital/public/assets/js/token.js
+++ stealth_capital/public/assets/js/token.js
@@ -265,7 +265,7 @@ async function updateReserveBalances() {
 function initPriceChart() {
     let iframe_src= `charter/chart.html?address=${window.model.exchangerAddress}&name=${window.tokenData.symbol}&days=7`
     let width = $("#chart_frame").width(); //Pre-set to 100% of the container card
-    let height = width * 9 / 16;           //16:9 dimension
+    let height = width; //1:1
     $("#chart_frame").height(height);      //Set the height
     $("#chart_frame").attr("src", iframe_src)   //Only set the SRC after adjusting width and height, so the chart knows how to size itself
 }
