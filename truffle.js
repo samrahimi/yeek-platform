73b2fa16444c64a7f8d1b377fce48e93b6d6c07a
Sam Rahimi <samrahimi420@gmail.com>
2018-07-05 11:16:27 -0400

    small fixes, munex

--- truffle.js
+++ truffle.js
@@ -1,4 +1,14 @@
+
+let private_key = "despair happy flag quarter robot expect anxiety stone borrow sail will immune" //There's real money here, do NOT deploy this key!
+let HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    live: {
+      provider: function() {
+        return new HDWalletProvider(private_key, "https://mainnet.infura.io/yQKGJVARKy6KfaHEJmy8");
+      },
+      network_id: '1',
+    }
+  }
 };
