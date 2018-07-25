d22441baf8a14704bb179116b8373f9ea9ce8a0f
prathamdesai <prathamdesai13@gmail.com>
2018-07-25 16:09:59 -0400

    Bot works

--- stealth_capital/public/TradingBot/trader.js
+++ stealth_capital/public/TradingBot/trader.js
@@ -10,25 +10,30 @@ var execution_time = null;
 var rand_event= null;
 var rand_eth = null;
 // min and max eth ranges
-const MIN_ETH = 0.0001;
-const MAX_ETH = 0.0002;
+const MIN_ETH = 0.01;
+const MAX_ETH = 0.05;
 // min and max time ranges in milliseconds
-const T1 = 5.0 * 1000 * 60; // 5 minutes in milliseconds
-const T2 = 15.0 * 1000 * 60; // 15 minutes in milliseconds
+const T1 = 3.0 * 1000 * 60; // 3 minutes in milliseconds
+const T2 = 6.0 * 1000 * 60; // 6 minutes in milliseconds
 // min and max token values
-const MIN_TOKENS = 0.0;
-var MAX_TOKENS;
 
 // INFO FOR TRADER BOT ACCOUNT:
-const accAddr = '0x2f9874d6bcf0a73f0b4dc6204e30b092123a7e2d';
-const accPrivKey = '2C607306B087AB8D813ADCCB030C321874FF260BD749E2F156391A48C5108BB0'
+const accAddr = '0xF8008F03A0E6fE0cC316c9888d68D65587337013';
+const accPrivKey = 'ed227e33021e231d9d198673e33facd0f87a90b3baa0256f36e3c6eb4d972da2';
+//const accPrivKey = 'ed227e33021e231d9d198673e33facd0f87a90b3baa0256f36e3c6eb4d972da2';
 // define contract up top
 var contract = new web3.eth.Contract(exchangerABI, testAddress);
+var tokenContract = new web3.eth.Contract(tokenABI, tokenAddress);
 const acc = web3.eth.accounts.privateKeyToAccount(accPrivKey.indexOf('0x') === 0 ? accPrivKey : '0x' + accPrivKey);
 // samples random between min and max values
+
+// prints balance of account
+web3.eth.getBalance(accAddr).then(function(res) {
+    console.log("Balance in Eth: " + web3.utils.fromWei(res + '', 'ether'));
+});
 function sampleRandom(min, max){
 
-    return Math.random() * (max - min) + min;
+    return (Math.random() * (max - min) + min);
 
 }
 
@@ -56,38 +61,37 @@ function getUpdatedNonce(){
 // 
 async function executeEventsInfura(){
     var encodedData;
-    updateGasPrice();
-
+    var tx;
     if (rand_event == "BUY"){
         // buying an ERC20 token using eth
-        var val = web3.utils.toWei(rand_eth.toFixed(10) + '', 'ether')
-        var eth2tokens = await contract.methods.getPurchasePrice(val).call();
-        encodedData =  contract.methods.buy(eth2tokens).encodeABI();
+        var val = web3.utils.toWei(rand_eth.toFixed(10), 'ether')
+        encodedData =  contract.methods.buy(0).encodeABI();
         //console.log("Eth: " + rand_eth);
         //console.log("Address: " + acc.address);
-        
-    }else{
+        tx = {
+            from : accAddr,
+            to : testAddress,
+            value: val,
+            data : encodedData,
+            gasPrice: web3.utils.toWei(gasPrice.toFixed(1), 'gwei'),
+            gas: 200000 // random large value to set
+        }
+    }else if (rand_event == "SELL"){
         // selling ERC20 token for eth
-        var tokens2eth = await contract.methods.getSalePrice(rand_tokens);
-        encodedData = await contract.methods.receiveApproval(acc.address, tokens2eth["arguments"][0], tokenAddress, "0x00").encodeABI();
-
+        console.log("Random tokens to trade: " + rand_tokens);
+        encodedData = tokenContract.methods.approveAndCall(testAddress, rand_tokens, "0x00").encodeABI();
+        tx = {
+            from : accAddr,
+            to : tokenAddress,
+            data : encodedData,
+            gasPrice: web3.utils.toWei(gasPrice.toFixed(1), 'gwei'),
+            gas: 500000 // random large value to set
+        }
     }
-    
-    var tx = {
-        from : acc.address,
-        to : testAddress,
-        data : encodedData,
-        gasPrice: window.gasPrice,
-        gas: web3.utils.toWei(window.gasPrice.toFixed(1), 'gwei')
-    }
-    console.log("Gas: " + tx.gas);
+    console.log(JSON.stringify(tx, null, 2));
     //console.log(web3.eth.getBalance(acc.address));
     web3.eth.accounts.signTransaction(tx, acc.privateKey).then(signed => {
         var transaction = web3.eth.sendSignedTransaction(signed.rawTransaction);
-
-        transaction.on('confirmation', (confirmationNumber, receipt) => {
-            console.log("Confirmation Number: "  + confirmationNumber, receipt);
-        });
 
         transaction.on('transactionHash', hash => {
             console.log("Hash: " + hash);
@@ -101,46 +105,41 @@ async function executeEventsInfura(){
             console.log (error);
         });
 
-    });
-}
-// execute event through contract instance
-async function executeEvent(){
-    
-    if (rand_event == "BUY"){
-        var eth2tokens = await contract.methods.getPurchasePrice(rand_eth * Math.pow(10.0, 18)).call();
-        await contract.methods.buy(eth2tokens).send({from: testAddress});
-        return ;
-
-    }else{
-        // eth returned in wei
-        var tokens2eth = await contract.methods.getSalePrice(rand_tokens);
-        //console.log(tokens2eth["arguments"][0]);
-        await contract.methods.receiveApproval(testAddress, tokens2eth["arguments"][0], testAddress, "0x00").send({from: testAddress});
-        return ;
-    }
+    }).catch((ex) => {console.log(ex.toString())});
 }
 
 // run bot in execution time intervals
 execution_time = sampleRandom(T1, T2);
-var token_promise = contract.methods.getPurchasePrice(MAX_ETH * Math.pow(10.0, 18)).call();
-token_promise.then(function(result) {
-    MAX_TOKENS = result;
-});
-console.log(execution_time)
-/*
-setInterval(async() => {
+console.log("Execution time: " + execution_time)
+updateGasPrice();
 
-    rand_event = sampleEvent();
-    //rand_event = "BUY";
+setTimeout(() => {
+    setInterval(() => {
+
+        rand_event = sampleEvent();
+        rand_eth = sampleRandom(MIN_ETH, MAX_ETH);
+        if (rand_event == "SELL")
+        {
+            var token_promise = contract.methods.getPurchasePrice(decimalToRaw(rand_eth, 18)).call();
+            token_promise.then(function(result) {
+                rand_tokens = result;
+                executeEventsInfura()
+            });
+        } else {
+            executeEventsInfura()
+        }
+
+
+    }, execution_time); 
+}, 5000) 
+
+function tester()
+{
+    rand_event = "SELL"
     rand_eth = sampleRandom(MIN_ETH, MAX_ETH);
-    rand_tokens = sampleRandom(MIN_TOKENS, MAX_TOKENS);
-    await executeEventsInfura();
-
-}, execution_time); 
-*/
-
-rand_event = "BUY";
-rand_eth = sampleRandom(MIN_ETH, MAX_ETH);
-rand_tokens = sampleRandom(MIN_TOKENS, MAX_TOKENS);
-console.log(rand_eth);
-executeEventsInfura();
+    var token_promise = contract.methods.getPurchasePrice(decimalToRaw(rand_eth, 18)).call();
+    token_promise.then(function(result) {
+        rand_tokens = result;
+        executeEventsInfura()
+    });
+}
